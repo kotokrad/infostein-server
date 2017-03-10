3d27cea83ce37dfff9fe3c752aa11e651c43a08e
Kotokrad <dsl2407@gmail.com>
2017-03-11 01:49:08 +0200

    Initial commit

---
+++ parser/parser.js
@@ -0,0 +1,113 @@
+const fs = require('fs');
+const url = require('url');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const moment = require('moment');
+const cache = require('memory-cache');
+
+const configs = require('./configs.json');
+
+function typify(textSource) {
+  var paragraph;
+  var match;
+  if (match = /^(.+),(.+): ?«(.+)»\.$/.exec(textSource)) {
+    var fullName = match[1].split(' ');
+    var firstName = fullName[0][0].toUpperCase() + fullName[0].slice(1).toLowerCase();
+    var lastName = fullName[1] ? ' ' + fullName[1].toUpperCase() : '';
+    var whois = match[2].trim();
+    var text = match[3];
+    paragraph = `СИНХРОН: ${firstName}${lastName}, ${whois}: «${text}».`;
+  } else if (match = /^ ?[-–] (.+)$/.exec(textSource)) {
+    var text = match[1];
+    paragraph = `СИНХРОН: _______: «${text}».`;
+  } else if (match = /^.+$/.exec(textSource)) {
+    var text = match[0];
+    paragraph = `РЕПОРТАЖ: ${text}`;
+  } else {
+    console.log('Error: Unable to parse paragraph:\n' + textSource);
+    return '';
+  }
+  return paragraph;
+}
+
+function normalize(textSource) {
+  var replaces = [
+    { str: '—', to: '–' },
+    { str: ' - ', to: ' – ' },
+    { str: ' "', to: ' «'},
+    { str: '" ', to: '» '},
+    { str: /"(\S)/g, to: '»$1'},
+    { str: /(\S)"/g, to: '$1»'},
+  ];
+  var text = textSource.trim();
+  replaces.forEach(function(replace) {
+    text = text.replace(new RegExp(replace.str, "g"), replace.to );
+  });
+  return text;
+};
+
+function loadSource(data, config) {
+  var $ = cheerio.load(data);
+  var title = $(config['sel:title']).text();
+  var body = $(config['sel:body']).text();
+  var date = $(config['sel:date']).text();
+  return {
+    title,
+    body,
+    date,
+  };
+}
+
+function parseItem(source, config) {
+  var item = {
+    channel: config.channel,
+    program: config.program,
+    hasRepeat: !!config.repeat,
+    repeat: config.repeat,
+  };
+  moment.locale('ru');
+  item.date = moment(source.date, config['match:date']).format('DD.MM.YYYY');
+  item.time = config.time;
+  var titleMatchResult = source.title.match(new RegExp(config['match:title'], ''))
+  item.title = normalize(titleMatchResult ? titleMatchResult[1] : source.title);
+  item.body = source.body
+    .replace(/\r?\n|\r/g, '\n')
+    .replace(/\n+/g, '#NEWLINE#')
+    .replace(/[\s\h]{2,}/g, ' ')
+    .split('#NEWLINE#')
+    .filter(p => !/^\s*$/m.test(p))
+    .map(p => normalize(p))
+    .map(p => {
+      return { paragraph: typify(p) };
+    });
+  return item;
+}
+
+function getItem(link) {
+  return new Promise((resolve, reject) => {
+    var item = cache.get(link);
+    if (item) {
+      resolve(item)
+    } else {
+      axios.get(link).then(({ data }) => {
+        var host = url.parse(link).host;
+        var config = configs[host];
+        var source = loadSource(data, config);
+        var item = parseItem(source, config);
+        cache.put(link, item, 30000, key => console.log(`deleted: ${key}`));
+        resolve(item)
+      }).catch(err => {
+        console.log(err);
+      });
+    }
+  });
+}
+
+function getItems(links) {
+  var promises = links.map(link => getItem(link));
+  return Promise.all(promises);
+}
+
+module.exports = {
+  getItems,
+}
---
+++ server.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
+const moment = require('moment');
+
+const parser = require('./parser/parser');
+
+const port = process.env.PORT || 4000;
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+app.use(express.static(__dirname + '/public'));
+app.use((req, res, next) => {
+  var now = moment().format('DD/MM/YYYY HH:mm');
+
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server log');
+    }
+  });
+  next();
+});
+
+app.post('/items', function(req, res) {
+    console.log(req.body);
+    console.time('parsing');
+    parser.getItems(req.body.links)
+      .then(items => {
+        console.timeEnd('parsing');
+        res.send(items);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+});
+
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
